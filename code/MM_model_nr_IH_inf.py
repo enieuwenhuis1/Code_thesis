879acc1a0069100694207c5dd72bbc09ecc02319
enieuwenhuis1 <152196575+enieuwenhuis1@users.noreply.github.com>
2024-03-24 16:38:29 +0100

    Update MM_model_nr_IH_inf.py

--- code/MM_model_nr_IH_inf.py
+++ code/MM_model_nr_IH_inf.py
@@ -2960,14 +2960,14 @@ def Figure_continuous_MTD_vs_AT(n_switches, t_steps_drug):
     axs[1, 2].set_ylabel(' ')
     axs[1, 2].set_title(r"Adaptive therapy MMd GF IH and $W_{MMd}$ IH")
     axs[1, 2].grid(True)
-    save_Figure(plt, 'line_plot_cell_nr_IH_inf_AT_MTD',
-                                 r'..\visualisation\results_model_nr_IH_inf')
 
     # Create a single legend outside of all plots
     legend_labels = ['Number of OC', 'Number of OB', 'Number of MMd',
                                                                 'Number of MMr']
     fig.legend(labels = legend_labels, loc='upper center', ncol=4,
                                                                 fontsize='large')
+    save_Figure(plt, 'line_plot_cell_nr_IH_inf_AT_MTD',
+                                 r'..\visualisation\results_model_nr_IH_inf')
     plt.show()
 
 """ Figure to determine the difference between traditional and adaptive therapy.
@@ -3111,13 +3111,13 @@ def Figure_continuous_MTD_vs_AT_short_a_h(n_switches, t_steps_drug):
     axs[1, 2].set_ylabel(' ')
     axs[1, 2].set_title(r"Adaptive therapy MMd GF IH and $W_{MMd}$ IH")
     axs[1, 2].grid(True)
-    save_Figure(plt, 'line_plot_cell_nr_IH_inf_AT_MTD_short_a_h',
-                                 r'..\visualisation\results_model_nr_IH_inf')
 
     # Create a single legend outside of all plots
     legend_labels = ['Number of OC', 'Number of OB', 'Number of MMd',
                                                                 'Number of MMr']
     fig.legend(labels = legend_labels, loc='upper center', ncol=4, fontsize='large')
+    save_Figure(plt, 'line_plot_cell_nr_IH_inf_AT_MTD_short_a_h',
+                                 r'..\visualisation\results_model_nr_IH_inf')
     plt.show()
 
 """ Figure to determine the difference between traditional and adaptive therapy
@@ -3261,14 +3261,14 @@ def Figure_continuous_MTD_vs_AT_weak_a_h(n_switches, t_steps_drug):
     axs[1, 2].set_ylabel(' ')
     axs[1, 2].set_title(r"Adaptive therapy MMd GF IH and $W_{MMd}$ IH")
     axs[1, 2].grid(True)
-    save_Figure(plt, 'line_plot_cell_nr_IH_inf_AT_MTD_weak_a_h',
-                                 r'..\visualisation\results_model_nr_IH_inf')
 
     # Create a single legend outside of all plots
     legend_labels = ['Number of OC', 'Number of OB', 'Number of MMd',
                                                                 'Number of MMr']
     fig.legend(labels = legend_labels, loc='upper center', ncol=4,
                                                                 fontsize='large')
+    save_Figure(plt, 'line_plot_cell_nr_IH_inf_AT_MTD_weak_a_h',
+                                 r'..\visualisation\results_model_nr_IH_inf')
     plt.show()
 
 
